d843a9a78cba779cf52f26d8e7ac0801340448af
Leonar Estupiñan Quiñonez <LeonarEQ@MacBook-Pro-de-Leonar.local>
2024-06-01 09:10:57 +0200

    Desarrollados métodos Biblioteca

--- Sistema-de-Gestión-de-Biblioteca/script.js
+++ Sistema-de-Gestión-de-Biblioteca/script.js
@@ -76,37 +76,62 @@ class Biblioteca {
 
   agregarLibro(libro) {
     // Añade un nuevo libro al catálogo de la biblioteca.
+    this.libros.push(libro);
+
+    return "'" + libro.titulo + "'" + " agregado con éxito";
   }
   agregarUsuario(usuario) {
     // Registra un nuevo usuario en la biblioteca.
+    this.usuarios.push(usuario);
+
+    return "Usuario: " + usuario.nombre + " (agregado con éxito)";
   }
   prestarLibro(usuario, libro) {
     // Permite a un usuario prestar un libro, si está disponible.
+    if (libro.estado === "Disponible") {
+      usuario.prestarLibro(libro);
+      libro.prestar();
+    } else {
+      console.log("Libro no disponible");
+    }
+
+    return usuario.mostrarLibrosPrestados();
   }
   devolverLibro(usuario, libro) {
     // Permite a un usuario devolver un libro prestado.
+    usuario.devolverLibro(libro);
+    libro.estado = "Disponible";
+    return "Libro: " + "'" + libro.titulo + "'" + " ha sido devuelto";
   }
   mostrarLibrosDisponibles() {
     // Muestra la información de todos los libros disponibles en la biblioteca.
+    return this.libros
+      .filter((element) => element.estado == "Disponible")
+      .map((key) => ({ Titulo: key.titulo, Autor: key.autor }));
   }
-  mostrarUsuario() {
+
+  mostrarUsuarios() {
     // Muestra la información de todos los usuarios registrados en la biblioteca.
+
+    return this.usuarios.map((key) => console.log(key));
   }
 }
 
+// Métodos de Libro
 const nuevolibro = new Libro(
   "Lo que el viento se llevo",
   "Margaret Mitchell",
   "ABC12345",
-  "Disponible"
+  "Ocupado"
 );
 
-/*console.log(nuevolibro);
+console.log(nuevolibro);
 console.log(nuevolibro.prestar());
 console.log(nuevolibro.devolver());
 console.log(nuevolibro.mostrarInfo());
-console.log(nuevolibro);*/
+console.log(nuevolibro);
 
+// Métodos de Usuario
 const nuevoUsuario = new Usuario("Pedro", "Y5640683Q");
 console.log(nuevoUsuario);
 console.log(nuevoUsuario.prestarLibro(nuevolibro));
@@ -121,5 +146,23 @@ console.log(
   )
 );
 console.log(nuevoUsuario.mostrarLibrosPrestados());
-
 console.log(nuevoUsuario);
+
+// Métodos de Biblioteca
+const nuevaBiblioteca = new Biblioteca("Casa del Libro");
+console.log(nuevaBiblioteca.agregarLibro(nuevolibro));
+console.log(
+  nuevaBiblioteca.agregarLibro(
+    new Libro(
+      "Padre Rico Padre Pobre",
+      "Robert Kiyosaki",
+      "DFG54321",
+      "Disponible"
+    )
+  )
+);
+console.log(nuevaBiblioteca.agregarUsuario(nuevoUsuario));
+console.log(nuevaBiblioteca.prestarLibro(nuevoUsuario, nuevolibro));
+console.log(nuevaBiblioteca.devolverLibro(nuevoUsuario, nuevolibro));
+console.log(nuevaBiblioteca.mostrarLibrosDisponibles());
+console.log(nuevaBiblioteca.mostrarUsuarios());
