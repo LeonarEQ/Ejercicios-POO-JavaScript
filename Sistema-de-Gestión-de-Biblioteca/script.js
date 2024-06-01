8fb348a22d0fe4680b18d94447c9b5488439ca32
Leonar Estupiñan Quiñonez <LeonarEQ@MacBook-Pro-de-Leonar.local>
2024-06-01 04:01:55 +0200

    Resueltos métodos Libro

--- Sistema-de-Gestión-de-Biblioteca/script.js
+++ Sistema-de-Gestión-de-Biblioteca/script.js
@@ -9,43 +9,62 @@ class Libro {
   }
 
   prestar() {
+    this.estado = "Prestado";
     // Marca el libro como prestado.
+    return "El libro ahora está: " + this.estado;
   }
 
   devolver() {
     // Marca el libro como disponible.
+    this.estado = "Disponible";
+    return "El libro ahora está: " + this.estado;
   }
 
   mostrarInfo() {
     // Muestra la información completa del libro.
+    return (
+      "Título: " +
+      this.titulo +
+      "\nAutor: " +
+      this.autor +
+      "\nISBN: " +
+      this.isbn +
+      "\nEstado: " +
+      this.estado
+    );
   }
 }
 
 class Usuario {
-  constructor(nombre, id, librosPrestados) {
+  constructor(nombre, id) {
     this.nombre = nombre;
     this.id = id;
-    this.librosPrestados = []; // array
   }
+
+  librosPrestados = [];
 
   prestarLibro(libro) {
     // Añade un libro a la lista de libros prestados del usuario.
+    this.librosPrestados.push(libro);
   }
 
   devolverLibro(libro) {
     // Elimina un libro de la lista de libros prestados del usuario.
+    delete this.librosPrestados(libro);
   }
   mostrarLibrosPrestados() {
     // Muestra la información de todos los libros prestados por el usuario.
+    console.log(this.librosPrestados);
   }
 }
 
 class Biblioteca {
-  constructor(nombre, libros, usuarios) {
+  constructor(nombre) {
     this.nombre = nombre;
-    this.libros = []; // array
-    this.usuarios = []; //array
   }
+
+  libros = [];
+  usuarios = [];
 
   agregarLibro(libro) {
     // Añade un nuevo libro al catálogo de la biblioteca.
@@ -66,3 +85,14 @@ class Biblioteca {
     // Muestra la información de todos los usuarios registrados en la biblioteca.
   }
 }
+const nuevolibro = new Libro(
+  "Lo que el viento se llevo",
+  "Margaret Mitchell",
+  "ABC12345",
+  "Disponibles"
+);
+console.log(nuevolibro);
+console.log(nuevolibro.prestar());
+console.log(nuevolibro.devolver());
+console.log(nuevolibro.mostrarInfo());
+console.log(nuevolibro);
